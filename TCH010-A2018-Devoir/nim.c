bf6f64a4e0174fcc5c245eb2e77e803701eeec3b
dakinguyen <44127008+dakinguyen@users.noreply.github.com>
2018-11-17 09:38:30 -0500

    fixed nim_plateau_defragmenter

--- TCH010-A2018-Devoir/nim.c
+++ TCH010-A2018-Devoir/nim.c
@@ -21,7 +21,7 @@ void nim_plateau_init(int plateau[], int nb_colonnes) {
 	
 	int i;
 
-	for (i = 0; i <= nb_colonnes - 1; i++) {
+	for (i = 0; i < nb_colonnes; i++) {
 		plateau[i] = PLATEAU_MIN_PIECES + rand() / (RAND_MAX / PLATEAU_MAX_PIECES + 1);
 	}
 
@@ -32,24 +32,24 @@ void nim_plateau_supprimer_colonne(int plateau[], int *nb_colonnes, int col_a_su
 
 	int i;
 
-	for (i = col_a_supprimer; i <= *nb_colonnes - 1; i++) {
+	for (i = col_a_supprimer; i < *nb_colonnes; i++) {
 		plateau[i] = plateau[i + 1];
 	}		
 }
 
-//// Supprimer les colonnes vides du tableau
-//int nim_plateau_defragmenter(int plateau[], int *nb_colonnes) {
-//	
-//	int i;
-//
-//	for (i = 0; i <= *nb_colonnes - 1; i++) {
-//		if (plateau[i] == 0) {
-//			nim_plateau_supprimer_colonne(plateau[], *nb_colonnes, i);
-//		}
-//		*nb_colonnes = *nb_colonnes - 1;
-//	}
-//	return *nb_colonnes;
-//}
+// supprimer les colonnes vides du tableau
+int nim_plateau_defragmenter(int plateau[], int *nb_colonnes) {
+	
+	int i;
+
+	for (i = 0; i < *nb_colonnes; i++) {
+		if (plateau[i] == 0) {
+			nim_plateau_supprimer_colonne(plateau[nb_colonnes - 1], *nb_colonnes, i);
+		}
+		*nb_colonnes = *nb_colonnes - 1;
+	}
+	return *nb_colonnes;
+}
 
 // valider le choix de l'utilisateur et enlever la piece si elle fonctionne
 int nim_jouer_tour(int plateau[], int nb_colonnes, int colonne, int nb_pieces) {
